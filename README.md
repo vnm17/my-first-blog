d871a48ca5bc0869bf99b79582d077708da730f0
Viveka Mastandrea <viveka.mastandrea@gmail.com>
2017-07-10 12:34:44 -0700

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # my-first-blog
-I am making a Django blog.
+I am making a Django blog based on the Django Girls tutorial (https://tutorial.djangogirls.org/) as part of the Girls Who Code 2017 Summer Immersion Program.
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 
 
