9f97d1bd2be4de34e882f45ee8d4ddf0ff3f8509
Viveka Mastandrea <viveka.mastandrea@gmail.com>
2017-07-10 12:25:22 -0700

    fixing more imports

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
-from django.shortcuts import redirect
 
 
 
